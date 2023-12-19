177811d0717e0b2f23224832cf4541b0f7152955
Trần Đình Mạnh Cường <81034438+cuongtran2203@users.noreply.github.com>
2023-12-19 09:37:43 +0700

    Update convTmixarcface.py
    change output model

--- convTmixarcface.py
+++ convTmixarcface.py
@@ -14,17 +14,20 @@ class ConvTmixArcFace(nn.Module):
         # Tạo một mô hình mới chỉ chứa 9 khối đầu tiên
         self.feats = torch.nn.Sequential(*selected_layers)
         self.GAP=nn.AvgPool2d(5)
-        self.arcface=ArcMarginProduct(in_features=512,out_features=512)
+        
     def forward(self,x):
         feature=self.feats(x)
         emb=self.GAP(feature)
-        print(emb.shape)
+        # print(emb.shape)
         # emb_v=self.arcface(emb)
-        return emb
-if __name__=="__main__":
-    device = torch.device("cuda")
-    model=ConvTmixArcFace(model_pretrained="checkpoint_M.pth")
-    input = torch.Tensor(4, 3, 224, 224)
-    out=model(input)
-    print(out.shape)
+        emb=torch.squeeze(emb,dim=-1)
+        return torch.squeeze(emb,dim=-1)
+# if __name__=="__main__":
+#     device = torch.device("cuda")
+#     model=ConvTmixArcFace(model_pretrained="checkpoint_M.pth")
+#     input = torch.Tensor(4, 3, 224, 224)
+#     out=model(input)
+#     print(out.shape)
+
+
 
